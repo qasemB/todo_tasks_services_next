7f9cecee890320f82ba39bcfdb96c9f797e7ffb5
qasemB <mahdicmptr@gmail.com>
2024-09-21 11:53:41 +0330

    after ok cors - server

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "*", // Set your origin
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "*",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "*",
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
