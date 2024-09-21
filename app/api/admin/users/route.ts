e19e41da6d0d56a34f8cd77e630c956d8302f4ba
qasemB <mahdicmptr@gmail.com>
2024-09-21 04:47:12 +0330

    after ok mongo db

--- app/api/admin/users/route.ts
+++ app/api/admin/users/route.ts
@@ -6,7 +6,7 @@ import { ResponseClass } from '@/utils/response';
 
 export async function POST(request: Request) {
     const verified = verifyToken(request)
-    if (!verified) return new NextResponse('Unauthorized', { status: 401 });        
+    if (!verified) return new NextResponse('Unauthorized', { status: 401 });
 
     const decodedToken = getDecodedToken(request)
     if (decodedToken?.role !== "admin") return new ResponseClass().custom(403);
@@ -14,12 +14,10 @@ export async function POST(request: Request) {
     const data: CreateUserParamsType = await request.json();
     data.password = await hashPassword(data.password)
     try {
-        const user = await prisma.user.create({
-            data,
-        });
-        return new ResponseClass(user, true).created;
+        const user = await prisma.user.create({ data });
+        return new ResponseClass(user, true).created();
     } catch (error) {
-        return new ResponseClass(null, false, 'Error creating user').custom(500);
+        return new ResponseClass(error, false, 'Error creating user').custom(500);
     }
 }
 
@@ -30,7 +28,7 @@ export async function GET(request: Request) {
 
     const decodedToken = getDecodedToken(request)
     if (decodedToken?.role !== "admin") return new ResponseClass().custom(403);
-    
+
     try {
         const users = await prisma.user.findMany();
         return new ResponseClass(users, true).success();
